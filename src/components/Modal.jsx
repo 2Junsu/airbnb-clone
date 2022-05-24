39f2b8db22d5b30a90091327eac75a75592a2141
2Junsu <ijh1205@naver.com>
2022-05-24 22:03:57 +0900

    Design: Modal design 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ModalComponent from "react-modal";
 
 const Modal = (props) => {
-    const { children, width, height, modalStatus, closeModal } = props;
+    const { children, width, modalStatus, closeModal } = props;
+
     return (
         <ModalComponent
+            id="modal"
             isOpen={modalStatus}
             onRequestClose={closeModal}
             style={{
@@ -12,17 +14,19 @@ const Modal = (props) => {
                     position: "fixed",
                     top: 0,
                     left: 0,
+                    backgroundColor: "rgba(0,0,0,0.5)",
                 },
                 content: {
+                    padding: 0,
                     position: "absolute",
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
-                    justifyContent: "center",
-                    top: `calc(50% - ${height / 2}px)`,
+                    // top: `calc(50% - ${height / 2}px)`,
                     left: `calc(50% - ${width / 2}px)`,
                     width: width + "px",
-                    height: height + "px",
+                    height: "fit-content",
+                    border: "none",
                     borderRadius: 12,
                     backgroundColor: "white",
                 },
