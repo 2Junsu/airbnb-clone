e0bcd8b67585818d83bd9aff09cf84078615d0dc
2Junsu <ijh1205@naver.com>
2022-05-24 22:04:20 +0900

    회원가입 컴포넌트 분리 및 main signup modal 구현

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,4 +3,14 @@ import CategoryItem from "./CategoryItem";
 import Header from "./Header";
 import Review from "./Review";
 import Modal from "./Modal";
-export { LodgingCard, CategoryItem, Header, Review, Modal };
+import SignupModal from "./SignupModal";
+import Signup from "./Signup";
+export {
+    LodgingCard,
+    CategoryItem,
+    Header,
+    Review,
+    Modal,
+    SignupModal,
+    Signup,
+};
