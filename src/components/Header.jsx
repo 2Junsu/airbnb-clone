b299ff7eb272d305ec440fdb48fc5dd8f2ae4ee4
2Junsu <ijh1205@naver.com>
2022-05-24 19:56:46 +0900

    Feat: Header nav modal 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../css/header.css";
 import logo from "../assets/imgs/airbnb.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+    const [openMemberInfo, setOpenMemberInfo] = useState(false);
+    const memberRef = useRef();
+
     return (
         <header>
-            <div>
+            <div className="header-left-container">
                 <Link to="/">
                     <img width="102px" height="32px" src={logo} alt="logo" />
                 </Link>
@@ -50,7 +53,7 @@ const Header = () => {
             <div className="navbar">
                 <nav>
                     <div className="nav-elem">
-                        <div>호스트 되기</div>
+                        <div className="host-btn">호스트 되기</div>
                         <div className="nav-lang">
                             <svg
                                 viewBox="0 0 16 16"
@@ -68,7 +71,11 @@ const Header = () => {
                             </svg>
                         </div>
                     </div>
-                    <div className="nav-elem2">
+                    <div
+                        className="nav-elem2"
+                        onClick={() => {
+                            setOpenMemberInfo(!openMemberInfo);
+                        }}>
                         <div>
                             <svg
                                 viewBox="0 0 32 32"
@@ -108,6 +115,27 @@ const Header = () => {
                                 <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
                             </svg>
                         </div>
+                        {openMemberInfo && (
+                            <div
+                                className="memberInfoContainer"
+                                ref={memberRef}>
+                                <div
+                                    style={{
+                                        paddingBottom: 8,
+                                        borderBottom: "1px solid #cccccc",
+                                    }}>
+                                    <button style={{ fontWeight: "bold" }}>
+                                        회원 가입
+                                    </button>
+                                    <button>로그인</button>
+                                </div>
+                                <div style={{ paddingTop: 8 }}>
+                                    <button>숙소 호스트 되기</button>
+                                    <button>체험 호스팅하기</button>
+                                    <button>도움말</button>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </nav>
             </div>
