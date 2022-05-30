94ab16c140417bb40a6f9763722c475d7057b796
2Junsu <ijh1205@naver.com>
2022-05-30 16:45:08 +0900

    Fix: LodgingCard 내의 carousel의 화살표 및 하트 버튼을 눌러도 페이지가 이동하던 이슈 해결

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect, useRef, Fragment } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../css/carousel.css";
 import leftArrow from "../assets/imgs/leftArrow.png";
 import rightArrow from "../assets/imgs/rightArrow.png";
+import { useNavigate } from "react-router-dom";
 
 const Carousel = (props) => {
-    const { images } = props;
-    const ref = useRef();
+    const { images, id } = props;
+    const carouselRef = useRef();
+    const ref = useRef([]);
+    const navigate = useNavigate();
+
     const [curSlide, setCurSlide] = useState(0);
     const totalSlide = images.length - 1;
-    const width = ref.current?.scrollWidth;
+    const width = carouselRef.current?.scrollWidth;
 
     const moveLeft = () => {
         if (curSlide === 0) setCurSlide(totalSlide);
@@ -21,17 +25,35 @@ const Carousel = (props) => {
     };
 
     useEffect(() => {
-        ref.current.style.transition = "all 0.5s ease-in-out";
-        ref.current.style.transform = `translate(${
+        carouselRef.current.style.transition = "all 0.5s ease-in-out";
+        carouselRef.current.style.transform = `translate(${
             (-width / images.length) * curSlide
         }px)`;
     }, [curSlide]);
+
+    const Routing = (e) => {
+        if (
+            !ref.current[0].contains(e.target) &&
+            !ref.current[1].contains(e.target) &&
+            !ref.current[2].contains(e.target) &&
+            carouselRef.current.contains(e.target)
+        )
+            navigate(`product/${id}`);
+    };
+
+    useEffect(() => {
+        window.addEventListener("click", Routing);
+
+        return () => {
+            window.removeEventListener("click", Routing);
+        };
+    });
 
     return (
         <div className="carousel-wrap">
             <div
                 className="carousel-container"
-                ref={ref}
+                ref={carouselRef}
                 style={{ width: `${images.length * 100}%` }}>
                 {images.map((data, idx) => (
                     <div key={idx} className="carousel-image-view">
@@ -42,6 +64,7 @@ const Carousel = (props) => {
                 ))}
             </div>
             <svg
+                ref={(e) => (ref.current[0] = e)}
                 viewBox="0 0 32 32"
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
@@ -62,12 +85,14 @@ const Carousel = (props) => {
                 <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path>
             </svg>
             <button
+                ref={(e) => (ref.current[1] = e)}
                 className="carousel-btn"
                 style={{ left: 10 }}
                 onClick={moveLeft}>
                 <img src={leftArrow} alt="arrow" width="20px" height="20px" />
             </button>
             <button
+                ref={(e) => (ref.current[2] = e)}
                 className="carousel-btn"
                 style={{ right: 10 }}
                 onClick={moveRight}>
